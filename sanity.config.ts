7438ddc8cbfd486a7f752322245e6bd3125e6be1
vinodchelmeda <vinod.chelmeda@freyrsolutions.com>
2026-07-16 20:21:59 +0530

    sanity

--- sanity.config.ts
+++ sanity.config.ts
@@ -15,7 +15,7 @@ import { structure } from "./sanity/deskStructure";
 
 export default defineConfig({
   basePath: "/admin",
-  projectId: projectId || "missing-project-id",
+  projectId: projectId || "wbvlv3m9s",
   dataset: dataset || "production",
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
